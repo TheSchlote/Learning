931cbbd4ac8f620d72e08984f58be5a9d1fba64f
TheSchlote <TheSchlote@gmail.com>
2020-01-22 19:38:15 -0600

    Chapter1
    Creation and initial push. Pushed after Chapter 1 of the book.

---
+++ Learning/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int characterLevel = 32;
+
+        int nextSkillLevel = GenerateCharacter("Spike", characterLevel);
+        Debug.Log(nextSkillLevel);
+        Debug.Log(GenerateCharacter("Faye", characterLevel));
+    }
+
+    public int GenerateCharacter(string name, int level)
+    {
+        return level + 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

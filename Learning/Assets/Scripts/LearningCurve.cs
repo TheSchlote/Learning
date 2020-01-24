a03158e93cdc2580ee1877fdc99399ca41afa934
TheSchlote <TheSchlote@gmail.com>
2020-01-23 20:48:31 -0600

    Finished Section 1
    More code will come...

--- Learning/Assets/Scripts/LearningCurve.cs
+++ Learning/Assets/Scripts/LearningCurve.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
+    private Transform camTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        int characterLevel = 32;
+        camTransform = this.GetComponent<Transform>();
+        Debug.Log(camTransform.localPosition);
 
-        int nextSkillLevel = GenerateCharacter("Spike", characterLevel);
-        Debug.Log(nextSkillLevel);
-        Debug.Log(GenerateCharacter("Faye", characterLevel));
+        //Character hero = new Character();
+        //Character hero2 = hero;
+        //hero2.name = "Joey2";
+        //hero.PrintStatsInfor();
+        //hero2.PrintStatsInfor();
+
+        //Character heroine = new Character("Destiny");
+        //heroine.PrintStatsInfor();
+
+        //Weapon huntingBow = new Weapon("Hunting Bow", 105);
+        //Weapon warBow = huntingBow;
+
+        //Paladin knight = new Paladin("Sir Arthur", huntingBow);
+        //knight.PrintStatsInfor();
     }
 
-    public int GenerateCharacter(string name, int level)
-    {
-        return level + 5;
-    }
 
     // Update is called once per frame
     void Update()
